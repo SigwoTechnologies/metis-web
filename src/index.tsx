ed575f50553440b3bb09e7bd1aec4591e7e74f42
Manuel Miranda <manuel.miranda@stackitgroup.com>
2022-07-01 15:32:51 -0700

    feat(setup): Added Material UI packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import CssBaseline from '@mui/material/CssBaseline';
+import { StyledEngineProvider } from '@mui/material/styles';
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from './store';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ReduxProvider store={store}>
-      <App />
-    </ReduxProvider>
+    <CssBaseline />
+    <StyledEngineProvider injectFirst>
+      <ReduxProvider store={store}>
+        <App />
+      </ReduxProvider>
+    </StyledEngineProvider>
   </React.StrictMode>
 );
