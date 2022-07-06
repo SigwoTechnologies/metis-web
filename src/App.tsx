0622e29c26e6cfaf30c1b13a3a20c7b562759bca
Manuel Miranda <manuel.miranda@stackitgroup.com>
2022-07-06 09:12:58 -0700

    fix(layout): remove useless layout folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // import Login from './pages/login/Login';
 
-import Main from './common/components/layout/main/Main';
+import Main from './common/components/layout/Main';
 
 const App = () => <Main />;
 
