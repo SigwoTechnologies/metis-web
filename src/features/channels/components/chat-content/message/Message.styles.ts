608056499edc7b92b34fedb6507db12f693bef3a
Jose Theis <jose.sanchez@stackitgroup.com>
2022-07-15 19:04:10 -0400

    feat: adding space between avatar and message and putting the avatar on the right side for the user

--- src/features/channels/components/chat-content/message/Message.styles.ts
+++ src/features/channels/components/chat-content/message/Message.styles.ts
@@ -6,10 +6,12 @@ const useStyles = makeStyles((theme: Theme) =>
     container: {
       display: 'flex',
       marginTop: '1.5rem',
+      gap: '0.75rem',
     },
     userContainer: {
       alignSelf: 'end',
       justifyContent: 'end',
+      flexDirection: 'row-reverse',
     },
     avatarContainer: {
       display: 'flex',
