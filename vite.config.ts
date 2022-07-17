09e7e1b0a8fe3b2a1e9fd25baa4336f72ddd9445
Jose Theis <jose.sanchez@stackitgroup.com>
2022-07-17 18:42:49 -0400

    fix: tests and lint-staged work now

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
 import { fileURLToPath, URL } from 'url';
+import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [eslintPlugin(), react()],
+  plugins: [eslintPlugin(), react(), EnvironmentPlugin('all')],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
