792a170c5a1643484d08c6ac7f6e4a96a8a1142e
Jose Theis <jose.sanchez@stackitgroup.com>
2022-07-21 21:11:48 -0400

    fix: refactoring code and adding margin

--- src/features/channels/components/chat-content/message/Message.tsx
+++ src/features/channels/components/chat-content/message/Message.tsx
@@ -44,7 +44,7 @@ const Message = ({
           </Box>
 
           <Box
-            className={isYours ? `${classes.messageContainer}` : classes.messageContainer}
+            className={classes.messageContainer}
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
           >
