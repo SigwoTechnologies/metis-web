97af737ded2cb0804affd910c013de9b228a4631
Jose Theis <jose.sanchez@stackitgroup.com>
2022-07-28 10:04:30 -0400

    fix: refactoring code and adding EmptyStateChannel component

--- src/pages/chat-page/ChatPage.tsx
+++ src/pages/chat-page/ChatPage.tsx
@@ -1,11 +1,11 @@
 import ChatContent from '@metis/features/channels/components/chat-content/ChatContent';
 import ChatFooter from '@metis/features/channels/components/chat-footer/ChatFooter';
 import ChatHeader from '@metis/features/channels/components/chat-header/ChatHeader';
+import EmptyStateChannel from '@metis/features/channels/components/empty-state-channel/EmptyStateChannel';
 import FooterSection from '@metis/layout/sections/FooterSection';
 import HeaderSection from '@metis/layout/sections/HeaderSection';
 import MainSection from '@metis/layout/sections/MainSection';
 import { useAppSelector } from '@metis/store/hooks';
-import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
 
 const ChatPage = () => {
   const { selectedChannel } = useAppSelector((state) => state.channel);
@@ -21,15 +21,7 @@ const ChatPage = () => {
           </MainSection>
         </>
       )}
-      {/* TODO: change these inline style to the stylesheet */}
-      {!selectedChannel && (
-        <div style={{ display: 'flex', flex: 1, color: 'white' }}>
-          <div style={{ margin: 'auto' }}>
-            <QuestionAnswerIcon />
-            <p>Select a channel and start chating!</p>
-          </div>
-        </div>
-      )}
+      {!selectedChannel && <EmptyStateChannel />}
       <FooterSection>
         <ChatFooter />
       </FooterSection>
