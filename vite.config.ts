d875aab0cab53338b27d08ac255746d747bab0f9
Jose Theis <jose.sanchez@stackitgroup.com>
2022-07-28 14:20:59 -0400

    fix: fixing bug with jest and import.meta in windows

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslintPlugin from 'vite-plugin-eslint';
 import { fileURLToPath, URL } from 'url';
-import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [eslintPlugin(), react(), EnvironmentPlugin('all')],
+  plugins: [eslintPlugin(), react()],
   resolve: {
     alias: {
       '@metis': fileURLToPath(new URL('./src', import.meta.url)),
