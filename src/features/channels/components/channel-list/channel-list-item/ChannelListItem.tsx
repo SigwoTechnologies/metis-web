e56f96f663105238d6d30eb2d2236ea8e966e99b
jonathanramirez <jonathan@stackitgroup.com>
2022-07-27 21:29:53 -0500

    feat(channels): adding hidden channels

--- src/features/channels/components/channel-list/channel-list-item/ChannelListItem.tsx
+++ src/features/channels/components/channel-list/channel-list-item/ChannelListItem.tsx
@@ -6,6 +6,7 @@ import ListItemButton from '@mui/material/ListItemButton';
 import Box from '@mui/material/Box';
 import DoneIcon from '@mui/icons-material/Done';
 import DoneAllIcon from '@mui/icons-material/DoneAll';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 import useStyles from './ChannelListItem.styles';
 
@@ -17,6 +18,7 @@ type Props = {
   avatar?: string;
   onClick?: () => void;
   selected?: boolean;
+  onHideChannel?: () => void;
 };
 
 const ChannelListItem = ({
@@ -27,45 +29,51 @@ const ChannelListItem = ({
   avatar = name,
   onClick,
   selected = false,
+  onHideChannel,
 }: Props) => {
   const classes = useStyles();
 
   return (
-    <ListItemButton onClick={onClick} alignItems="flex-start" selected={selected}>
-      <ListItemAvatar>
-        <Avatar alt={name} src={avatar} className={classes.avatar} />
-      </ListItemAvatar>
-      <ListItemText
-        primary={
-          <Box display="flex" justifyContent="space-between">
-            <Box className={classes.channelName}>
-              <Typography component="span" variant="caption" color="text.primary">
-                {name}
-              </Typography>
-            </Box>
-            <Box className={classes.channelDescription}>
-              <Box display="flex">
-                {isRead ? (
-                  <DoneAllIcon fontSize="small" color="primary" />
-                ) : (
-                  <DoneIcon fontSize="small" color="primary" />
-                )}
-              </Box>
-              <Box>
+    <Box display="flex" alignItems="center">
+      <ListItemButton onClick={onClick} alignItems="flex-start" selected={selected}>
+        <ListItemAvatar>
+          <Avatar alt={name} src={avatar} className={classes.avatar} />
+        </ListItemAvatar>
+        <ListItemText
+          primary={
+            <Box display="flex" justifyContent="space-between">
+              <Box className={classes.channelName}>
                 <Typography component="span" variant="caption" color="text.primary">
-                  {date}
+                  {name}
                 </Typography>
               </Box>
+              <Box className={classes.channelDescription}>
+                <Box display="flex">
+                  {isRead ? (
+                    <DoneAllIcon fontSize="small" color="primary" />
+                  ) : (
+                    <DoneIcon fontSize="small" color="primary" />
+                  )}
+                </Box>
+                <Box>
+                  <Typography component="span" variant="caption" color="text.primary">
+                    {date}
+                  </Typography>
+                </Box>
+              </Box>
             </Box>
-          </Box>
-        }
-        secondary={
-          <Typography component="span" variant="caption" color="text.secondary">
-            {message}
-          </Typography>
-        }
-      />
-    </ListItemButton>
+          }
+          secondary={
+            <Typography component="span" variant="caption" color="text.secondary">
+              {message}
+            </Typography>
+          }
+        />
+      </ListItemButton>
+      <Box display="flex">
+        <VisibilityOffIcon fontSize="small" color="primary" onClick={onHideChannel} />
+      </Box>
+    </Box>
   );
 };
 
--- src/features/channels/store/channel.actions.ts
+++ src/features/channels/store/channel.actions.ts
@@ -1,6 +1,35 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { Channel } from '@metis/features/channels/types/channel';
 import channelService from '../services/channel.service';
 
 export const findChannels = createAsyncThunk('channels/findChannels', channelService.findChannels);
 
-export default { findChannels };
+function hasStringJsonStructure(str: string | null) {
+  if (!str) return false;
+  try {
+    const result = JSON.parse(str);
+    const type = Object.prototype.toString.call(result);
+    return type === '[object Object]' || type === '[object Array]';
+  } catch (err) {
+    return false;
+  }
+}
+
+export const getHiddenChannels = () => {
+  const hiddenChannels = localStorage.getItem('@hiddenChannels');
+  return hasStringJsonStructure(hiddenChannels) ? JSON.parse(<string>hiddenChannels) : [];
+};
+
+export const hideChannel = (channel: Channel) => {
+  const currentHiddenChannels = getHiddenChannels();
+  const isChannelAlreadyHidden = currentHiddenChannels.find(
+    (chc: Channel) => chc?.channelAddress === channel.channelAddress
+  );
+
+  if (!isChannelAlreadyHidden) {
+    currentHiddenChannels.push(channel);
+    localStorage.setItem('@hiddenChannels', JSON.stringify(currentHiddenChannels));
+  }
+};
+
+export default { findChannels, hideChannel, getHiddenChannels };
