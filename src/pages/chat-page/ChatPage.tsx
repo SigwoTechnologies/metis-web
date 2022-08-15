d2831d0395c446fae047b1a3625aa3389952b24b
Jose Theis <jose.sanchez@stackitgroup.com>
2022-08-15 10:31:16 -0400

    fix: now you get redirected to the main page if the channels are empty

--- src/pages/chat-page/ChatPage.tsx
+++ src/pages/chat-page/ChatPage.tsx
@@ -5,16 +5,24 @@ import { selectChannel } from '@metis/features/channels/store/channel.slice';
 import FooterSection from '@metis/layout/sections/FooterSection';
 import HeaderSection from '@metis/layout/sections/HeaderSection';
 import MainSection from '@metis/layout/sections/MainSection';
-import { useAppDispatch } from '@metis/store/hooks';
+import { useAppDispatch, useAppSelector } from '@metis/store/hooks';
 import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const ChatPage = () => {
   const { channelAddress } = useParams();
+  const { channels } = useAppSelector((state) => state.channel);
+  const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
   // Set the selected channel when the user navigates to the page
   useEffect(() => {
+    // Navigate to the main page on start when there's no channels
+    if (channels.length === 0) {
+      navigate('/main');
+      return;
+    }
+
     dispatch(selectChannel(channelAddress));
   }, [channelAddress]);
 
