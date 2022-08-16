83be0b2abff39ebae6d4110fe1797024590b5dca
Jose Theis <jose.sanchez@stackitgroup.com>
2022-08-16 16:16:00 -0400

    feat(messages): adding new types, modifying Channel type and fixing tests

--- src/features/channels/types/channel.ts
+++ src/features/channels/types/channel.ts
@@ -1,7 +1,10 @@
+import { ChannelsMessagesResponse } from './ChannelsMessagesResponse';
+
 export type Channel = {
   channelAddress: string;
   channelPublicKey: string;
   channelName: string;
   createdBy: string;
   createdAt: number;
+  messages: ChannelsMessagesResponse[];
 };
