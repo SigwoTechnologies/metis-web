e24fbdad4c264d9109a70bb7ad2674cfb0073966
America Nishimoto <america.nishimoto@stackitgroup.com>
2022-08-24 16:35:16 -0700

    feat: added a customizable scrollbar

--- src/layout/sections/Section.styles.ts
+++ src/layout/sections/Section.styles.ts
@@ -6,6 +6,19 @@ const useStyles = makeStyles(() =>
       height: '5rem',
       position: 'relative',
     },
+    '@global': {
+      '*::-webkit-scrollbar': {
+        width: '0.4em',
+      },
+      '*::-webkit-scrollbar-track': {
+        '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)',
+      },
+      '*::-webkit-scrollbar-thumb': {
+        backgroundColor: 'rgba(0,0,0,.1)',
+        outline: '1px solid slategrey',
+      },
+    },
+
     main: {
       flex: 1,
       overflowY: 'auto',
