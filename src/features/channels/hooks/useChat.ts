450c1cd09e98cc41af12d9890a083cf9739daf79
Jose Theis <jose.sanchez@stackitgroup.com>
2022-08-30 17:05:34 -0400

    fix: changing static values to the user's

--- src/features/channels/hooks/useChat.ts
+++ src/features/channels/hooks/useChat.ts
@@ -1,4 +1,5 @@
 import connect from '@metis/common/services/socket.service';
+import { useAppSelector } from '@metis/store/hooks';
 import { useEffect, useRef } from 'react';
 import { Socket } from 'socket.io-client';
 import { Message } from '../types/Message';
@@ -9,13 +10,16 @@ type ChatSocketResponse = {
 
 export default (channelAddress: string) => {
   const socketRef = useRef<Socket>();
+  const {
+    jupAccount: { alias },
+  } = useAppSelector((state) => state.auth);
 
   useEffect(() => {
     if (channelAddress) {
       socketRef.current = connect({
         query: {
           room: channelAddress, // address of the current channel
-          user: 'user1', // TODO: change this for the alias of the current user
+          user: alias,
           event: 'createMessage',
         },
       }).socket('/chat');
