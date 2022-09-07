941f296dd23f491d6eb7f291c8241b78e2349948
AlefrankM <alefrank.martinez@stackitgroup.com>
2022-09-07 09:45:59 -0400

    fix: no all caps

--- src/features/channels/components/channel-list/create-channel/CreateButton.styles.ts
+++ src/features/channels/components/channel-list/create-channel/CreateButton.styles.ts
@@ -16,6 +16,7 @@ const useStyles = makeStyles((theme: Theme) =>
     button: {
       width: '100%',
       marginBottom: '1rem',
+      textTransform: 'capitalize',
     },
     textField: {
       width: '100%',
