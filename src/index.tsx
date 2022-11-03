df48dc3d058842e20f9ca023956c369681d8d190
AlefrankM <alefrank.martinez@stackitgroup.com>
2022-11-03 08:59:39 -0400

    feat: added swr transactions

--- src/index.tsx
+++ src/index.tsx
@@ -4,22 +4,28 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { StyledEngineProvider } from '@mui/material/styles';
 import { Provider as ReduxProvider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { SWRConfig } from 'swr';
 import store from './store';
 import App from './App';
 import ThemeProvider from './theme/ThemeProvider';
+import httpService from './common/services/http.service';
+
+const fetcher = async (url: string) => httpService(url).then(({ data }) => data);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <CssBaseline />
-    <StyledEngineProvider injectFirst>
-      <ReduxProvider store={store}>
-        <BrowserRouter>
-          <ThemeProvider>
-            <App />
-          </ThemeProvider>
-        </BrowserRouter>
-      </ReduxProvider>
-    </StyledEngineProvider>
+    <SWRConfig value={{ fetcher }}>
+      <CssBaseline />
+      <StyledEngineProvider injectFirst>
+        <ReduxProvider store={store}>
+          <BrowserRouter>
+            <ThemeProvider>
+              <App />
+            </ThemeProvider>
+          </BrowserRouter>
+        </ReduxProvider>
+      </StyledEngineProvider>
+    </SWRConfig>
   </React.StrictMode>
 );
